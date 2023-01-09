7a3552311aadd9d952e70b83c15227c07e402b09
eprei <emiprei@gmail.com>
2023-01-09 16:07:46 +0100

    add docker compose file and mariadb

--- srcs/requirements/mariadb/conf/start_config.sh
+++ srcs/requirements/mariadb/conf/start_config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-echo -e "\n\033[33mStart instalation mariadb\n\033[0m";
-mysql_install_db;
+echo -e "\n\033[33mStart instalation mariadb\n\033[0m"
+sleep 1
+mysql_install_db
 if [ $? == 0 ]; then
 	echo -e "\t\033[32mMaria db correctly installed\033[0m"
 else
@@ -8,8 +9,10 @@ else
 	exit 1
 fi
 
+sleep 1
 echo -e "\n\033[33mCreating directory /run/mysqld\n\033[0m";
-mkdir /run/mysqld;
+sleep 1
+mkdir /run/mysqld
 if [ $? == 0 ]; then
 	echo -e "\t\033[32mDirectory /run/mysqldMaria successfully created\033[0m"
 else
@@ -17,8 +20,10 @@ else
 	exit 1
 fi
 
-echo -e "\n\033[33mChanging directory /run/mysqld permissions\n\033[0m";
-chmod 777 /run/mysqld;
+sleep 1
+echo -e "\n\033[33mChanging directory /run/mysqld permissions\n\033[0m"
+sleep 1
+chmod 777 /run/mysqld
 if [ $? == 0 ]; then
 	echo -e "\t\033[32mPermissions successfully changed\033[0m"
 else
@@ -26,17 +31,10 @@ else
 	exit 1
 fi
 
-echo -e "\t\n\033[33mCreating mysqld users and passwords\n\033[0m";
-/usr/bin/mysqld --user=mysql --bootstrap << EOF
-FLUSH PRIVILEGES;
-ALTER USER root@localhost IDENTIFIED BY 'koko';
-CREATE DATABASE IF NOT EXISTS DATA_BASE_NAME;
-USE DATA_BASE_NAME;
-CREATE USER IF NOT EXISTS user@'%' IDENTIFIED BY 'koko';
-GRANT ALL PRIVILEGES ON DATA_BASE_NAME.* TO user@'%';
-CREATE USER IF NOT EXISTS USER2@'%' IDENTIFIED BY 'koko';
-FLUSH PRIVILEGES;
-EOF
+sleep 1
+echo -e "\t\n\033[33mExecuting comands.sql script to create mysqld users and passwords\n\033[0m"
+sleep 1
+/usr/bin/mysqld --user=mysql --bootstrap < comands.sql
 if [ $? == 0 ]; then
 	echo -e "\033[32mUsers and passwords successfully created\033[0m"
 else
@@ -44,29 +42,5 @@ else
 	exit 1
 fi
 
-echo -e "\t\n\033[33mRestarting mysql\n\033[0m";
-/usr/bin/mysqld;
-if [ $? == 0 ]; then
-	echo -e "\033[32mMysql successfully restarted\033[0m"
-else
-	echo -e "\033[31mMysql couldn't be restarted\033[0m"
-	exit 1
-fi
-
-# #!/bin/sh
-# mysql_install_db;
-# mkdir /run/mysqld;
-# chmod 777 /run/mysqld;
-
-# /usr/bin/mysqld --user=mysql --bootstrap << EOF
-# FLUSH PRIVILEGES;
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-# CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;
-# USE $DATABASE_NAME;
-# CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-# GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%';
-# CREATE USER IF NOT EXISTS '$SECOND_USER'@'%' IDENTIFIED BY '$SECOND_USER_PASSWORD';
-# FLUSH PRIVILEGES;
-# EOF
-
-# /usr/bin/mysqld;
+echo -e "\t\n\033[33mRestarting mysql\n\033[0m"
+/usr/bin/mysqld
