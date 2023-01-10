c43f7b0c3412dd81bcdf82b4fe88c21e3e5b1099
eprei <emiprei@gmail.com>
2023-01-10 18:08:14 +0100

    add docker-compose and makefile

---
+++ Makefile
@@ -0,0 +1,29 @@
+all :
+	mkdir -p /home/epresa/data/wordpress
+	mkdir -p /home/epresa/data/mariadb
+	make up
+
+up :
+	docker-compose -f ./srcs/docker-compose.yml up -d
+
+build :
+	docker-compose -f ./srcs/docker-compose.yml build
+
+stop :
+	docker-compose -f ./srcs/docker-compose.yml stop
+
+restart :
+	docker-compose -f ./srcs/docker-compose.yml restart
+
+remove :
+	docker-compose -f ./srcs/docker-compose.yml rm
+
+down :
+	docker-compose -f ./srcs/docker-compose.yml down --volumes --rmi all
+
+fclean : down
+	rm -rf /home/epresa/data/wordpress
+	sudo rm -rf /home/epresa/data/mariadb
+	docker system prune -f
+
+.PHONY: up build stop restart remove down fclean
--- srcs/requirements/mariadb/conf/start_config.sh
+++ srcs/requirements/mariadb/conf/start_config.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo -e "\n\033[33mStart instalation mariadb\n\033[0m"
-sleep 1
 mysql_install_db
 if [ $? == 0 ]; then
 	echo -e "\t\033[32mMaria db correctly installed\033[0m"
@@ -9,20 +8,22 @@ else
 	exit 1
 fi
 
-sleep 1
-echo -e "\n\033[33mCreating directory /run/mysqld\n\033[0m";
-sleep 1
-mkdir /run/mysqld
-if [ $? == 0 ]; then
-	echo -e "\t\033[32mDirectory /run/mysqldMaria successfully created\033[0m"
+if [ -d /run/mysqld ] ; then
+	echo -e "\n\033[33mThe directory /run/mysqld already exists\n\033[0m";
 else
-	echo -e "\t\033[31mDirectory /run/mysqldMaria couldn't be created\033[0m"
-	exit 1
+{
+	echo -e "\n\033[33mCreating directory /run/mysqld\n\033[0m";
+	mkdir /run/mysqld
+	if [ $? == 0 ]; then
+		echo -e "\t\033[32mDirectory /run/mysqldMaria successfully created\033[0m"
+	else
+		echo -e "\t\033[31mDirectory /run/mysqldMaria couldn't be created\033[0m"
+		exit 1
+	fi
+}
 fi
 
-sleep 1
 echo -e "\n\033[33mChanging directory /run/mysqld permissions\n\033[0m"
-sleep 1
 chmod 777 /run/mysqld
 if [ $? == 0 ]; then
 	echo -e "\t\033[32mPermissions successfully changed\033[0m"
@@ -31,9 +32,7 @@ else
 	exit 1
 fi
 
-sleep 1
 echo -e "\t\n\033[33mExecuting comands.sql script to create mysqld users and passwords\n\033[0m"
-sleep 1
 /usr/bin/mysqld --user=mysql --bootstrap < comands.sql
 if [ $? == 0 ]; then
 	echo -e "\033[32mUsers and passwords successfully created\033[0m"
