a977eb918be25e41c41d1136e6984c87f5057f75
eprei <emiprei@gmail.com>
2023-01-27 11:04:08 +0100

    clean mariadb/start_config.sh

--- srcs/requirements/mariadb/conf/start_config.sh
+++ srcs/requirements/mariadb/conf/start_config.sh
@@ -3,7 +3,7 @@ if [ ! -d /var/lib/mysql/mysql ]; then
 {
 	echo -e "\t\033[33m/var/lib/mysql/mysql not founded\033[0m"
 	echo -e "\n\033[33mStart instalation mariadb\n\033[0m"
-	mysql_install_db --datadir=/var/lib/mysql
+	mysql_install_db
 	if [ $? == 0 ]; then
 		echo -e "\t\033[32mMaria db correctly installed\033[0m"
 	else
@@ -18,32 +18,8 @@ if [ ! -d /var/lib/mysql/mysql ]; then
 		echo -e "\t\033[31m32mERROR: mysqld_safe has a problem during ths starting process\033[0m"
 		exit 1
 	fi
-	sleep 5
 
-	if [ -d /run/mysqld ] ; then
-		echo -e "\n\033[33mThe directory /run/mysqld already exists\033[0m";
-	else
-	{
-		echo -e "\n\033[33mCreating directory /run/mysqld\n\033[0m";
-		mkdir /run/mysqld
-		if [ $? == 0 ]; then
-			echo -e "\t\033[32mDirectory /run/mysqld successfully created\033[0m"
-		else
-			echo -e "\t\033[31mERROR: Directory /run/mysqld couldn't be created\033[0m"
-			exit 1
-		fi
-	}
-	fi
-
-	echo -e "\n\033[33mChanging directory /run/mysqld permissions\n\033[0m"
-	chmod 777 /run/mysqld
-	if [ $? == 0 ]; then
-		echo -e "\t\033[32mPermissions successfully changed\033[0m"
-	else
-		echo -e "\t\033[31mERROR: Permissions couldn't be changed\033[0m"
-		exit 1
-	fi
-
+	sleep 3
 	echo -e "\t\n\033[33mExecuting comands.sql script to create mysqld users and passwords\n\033[0m"
 	envsubst < comands.sql | mysql -h localhost -u root
 	if [ $? == 0 ]; then
@@ -55,5 +31,5 @@ if [ ! -d /var/lib/mysql/mysql ]; then
 }
 else
 	echo -e "\033[32mThe database was already created\033[30m"
-	/usr/bin/mysqld_safe --datadir=/var/lib/mysql
+	/usr/bin/mysqld_safe
 fi
